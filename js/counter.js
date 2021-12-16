2e2d8f8077f8d6def2b1e1ba8c4b9c78efd6c206
Thijs Boom <twboom@hotmail.nl>
2021-12-16 18:51:08 +0100

    Did things

--- js/counter.js
+++ js/counter.js
@@ -2,45 +2,79 @@ const startButton = document.getElementById('toggle');
 const timer = document.getElementById('timer');
 const clickCounter = document.getElementById('click-counter');
 
-let isRunning = false;
+let state = 'READY';
 
+let hits = [];
 let count = 0;
-let startTime = 0;
+let startTime = Date.now();
 
 let timeInterval;
+let timeTimeout;
 
-function toggle(evt) {
-    if (isRunning) {
-        if (evt instanceof Event) { stop(true); }
-        startButton.innerHTML = 'Start';
-    } else {
-        start();
-        startButton.innerHTML = 'Stop';
+function toggle(override) {
+    // if (isRunning) {
+    //     stop(override);
+    //     startButton.innerHTML = 'Reset';
+    // } else {
+    //     start();
+    //     startButton.innerHTML = 'Stop';
+    // }
+    // isRunning = !isRunning;
+
+    switch(state) {
+
+        case 'RUNNING': // Want it to stop
+            stop(override);
+            state = 'STOPPED'
+            break;
+
+        case 'STOPPED': // Want it to reset
+            reset()
+            state = 'READY'
+            break;
+
+        case 'READY': // Want it to start
+            start();
+            state = 'RUNNING'
+            break;
+
     }
-    isRunning = !isRunning;
 }
 
 function start() {
-    count = 0;
+    console.log('start')
+    startButton.innerHTML = 'Stop';
     startButton.blur()
     timeInterval = setInterval(updateTimer, 10);
     document.addEventListener('keydown', handleKeyDown);
     startTime = Date.now();
-    setTimeout(stop, config.time)
-    updateCounter();
+    timeTimeout = setTimeout(stop, config.time)
 };
 
 function stop(override) {
+    console.log('stop')
+    clearTimeout(timeTimeout);
     document.removeEventListener('keydown', handleKeyDown);
     clearInterval(timeInterval);
-    if (!override) { timer.innerHTML = config.time / 1000; };
-    toggle();
+    if (!override) {
+        timer.innerHTML = config.time / 1000;
+        toggle();
+    };
+    startButton.innerHTML = 'Reset';
 };
+
+function reset() {
+    startButton.innerHTML = 'Start';
+    count = 0;
+    updateCounter();
+    timer.innerHTML = '0';
+}
 
 function handleKeyDown(evt) {
     if (evt.key === ' ') {
         count++;
         updateCounter();
+        hits.push({ time: Date.now() - startTime, count: count });
     }
 }
 
