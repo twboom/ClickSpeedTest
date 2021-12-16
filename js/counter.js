f538b415b3a9eb801406745cab07d30f9fbacb90
Thijs Boom <twboom@hotmail.nl>
2021-12-16 18:17:32 +0100

    Things

--- js/counter.js
+++ js/counter.js
@@ -1,26 +1,59 @@
-const start = document.getElementById('start');
+const startButton = document.getElementById('toggle');
 const timer = document.getElementById('timer');
 const clickCounter = document.getElementById('click-counter');
 
-function run(time) {
-    let count = 0;
+let isRunning = false;
 
-    document.addEventListener('keydown', evt => {
-        if (evt.key === ' ') {
-            count++
-        }
-        updateCounter(count)
-    })
+let count = 0;
+let startTime = 0;
 
-    setTimeout(_ => {
+let timeInterval;
 
-    })
+function toggle(evt) {
+    if (isRunning) {
+        if (evt instanceof Event) { stop(true); }
+        startButton.innerHTML = 'Start';
+    } else {
+        start();
+        startButton.innerHTML = 'Stop';
+    }
+    isRunning = !isRunning;
 }
 
-function start() {};
+function start() {
+    count = 0;
+    startButton.blur()
+    timeInterval = setInterval(updateTimer, 10);
+    document.addEventListener('keydown', handleKeyDown);
+    startTime = Date.now();
+    setTimeout(stop, config.time)
+    updateCounter();
+};
 
-function stop() {};
+function stop(override) {
+    document.removeEventListener('keydown', handleKeyDown);
+    clearInterval(timeInterval);
+    if (!override) { timer.innerHTML = config.time / 1000; };
+    toggle();
+};
 
-function updateCounter(count) {
+function handleKeyDown(evt) {
+    if (evt.key === ' ') {
+        count++;
+        updateCounter();
+    }
+}
+
+async function updateCounter() {
     clickCounter.innerHTML = count;
+};
+
+async function updateTimer() {
+    timer.innerHTML = (Date.now() - startTime) / 1000;
 }
+
+function main() {
+    startButton.addEventListener('click', toggle)
+};
+
+main()
